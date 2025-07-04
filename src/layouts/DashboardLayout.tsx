3bb01b1dbad3c7676be92058f248a5c0a578ef29
Quixelone <lcoccimiglio@gmail.com>
2025-07-04 21:18:41 +0200

    Fix useEffect import in DashboardLayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import DashboardHeader from '@/components/dashboard/DashboardHeader';
 import DashboardSidebar from '@/components/dashboard/DashboardSidebar';
